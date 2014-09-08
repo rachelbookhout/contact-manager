587d81ec141b001b3232496a4fc057a334a2e84e
rachelbookhout <rbookhou@gmail.com>
2014-09-08 16:29:36 -0400

    Working Pagination

--- app.rb
+++ app.rb
@@ -7,12 +7,19 @@ require_relative 'models/contact'
 
 
 get '/' do
-if params[:query]
-  @contacts = Contact.where(first_name: params[:query]) || Contact.where(last_name: params[:query])
-else
-  @contacts = Contact.all
- end
-  erb :index
+  if params[:query]
+    @contacts = Contact.where(first_name: params[:query]) || Contact.where(last_name: params[:query])
+  else
+    @contacts = Contact.all
+  end
+
+  if params[:page] == nil || params[:page] == 0
+    @contacts = Contact.limit(5)
+  else
+    num = params[:page].to_i * 5
+    @contacts = Contact.limit(5).offset(num)
+  end
+erb :index
 end
 
 get '/contacts/:id' do
